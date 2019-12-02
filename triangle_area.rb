04f7ee9a7090d1744a4e8387e731957f69177646
Arina Zaitseva <avanero@bk.ru>
2019-12-02 20:50:57 +0700

    New commit

---
+++ triangle_area.rb
@@ -0,0 +1,7 @@
+puts "Основание треугольника?"
+a = gets.chomp.to_i
+puts "Высота треугольника?"
+h = gets.chomp.to_i
+area = 0.5*a*h
+puts "Площадь треугольника: #{area}"
+
---
+++ triangles.rb
@@ -0,0 +1,17 @@
+puts "Сторона а?"
+a = gets.chomp.to_i
+puts "Сторона b?"
+b = gets.chomp.to_i
+puts "Сторона с?"
+c = gets.chomp.to_i
+
+sides = [a,b,c]
+sides.sort!
+
+if (sides[0]**2) + (sides[1]**2) == (sides[2]**2)
+	puts "Треугольник прямоугольный"
+elsif sides[0] == sides[1]
+	puts "Треугольник равнобедренный"
+elsif sides[0] == sides[1] && sides[1] == sides[2]
+	puts "Треугольник равносторонний"
+end
